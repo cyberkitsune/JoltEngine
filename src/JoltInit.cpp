aba8c0866a7a2c6b1865f71f292f723967c6d41f
Sean McClenaghan <cyberkitsune09@gmail.com>
2013-02-05 19:03:01 -0800

    Added more debug console stuff, ideas for resource loading.

--- src/JoltInit.cpp
+++ src/JoltInit.cpp
@@ -19,5 +19,23 @@ bool JoltApp::doInit() {
         return false;
     }
     JoltConsole::logInfo("Init", "Screen initilized.");
+    
+    int numResources = 0;
+    JoltConsole::logInfo("Init", "Begin resource loading. Going to load %i resources", numResources);
+    int loadedResources = 0;
+    
+    /*
+     * Here we'll do resource discovery, prolly via a global resources.ini
+     * resources.ini example:
+     * player plr = player.ini
+     * map default = default.ini
+     * map level1 = l1.ini
+     * etc
+     * level inis will have entities? and will increment the resource counter.
+     */
+    
+    JoltConsole::logInfo("Init", "Resource loading complete. Loeaded %i resources successfully.", loadedResources);
+    
+    
     return true;
 }
