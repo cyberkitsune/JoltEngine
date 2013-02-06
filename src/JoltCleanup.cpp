d1f30b9ef58f47dd7bafd7839ec1521404ad0a84
Sean McClenaghan <cyberkitsune09@gmail.com>
2013-02-05 18:50:16 -0800

    Added pointless debug messages for cleanup.

--- src/JoltCleanup.cpp
+++ src/JoltCleanup.cpp
@@ -1,12 +1,19 @@
 #include "JoltApp.h"
+#include "JoltConsole.h"
 
 void JoltApp::doCleanup() {
     
+    JoltConsole::logInfo("Init", "Starting cleanup.");
+    
+    JoltConsole::logInfo("Cleanup", "Cleaning %i entities.", JoltEntity::entList.size());
     for (int i = 0; i < JoltEntity::entList.size(); i++) {
         if (!JoltEntity::entList[i]) continue; // Let's hope this never happens anyway
 
         JoltEntity::entList[i]->onCleanup();
     }
+    JoltConsole::logInfo("Cleanup", "Entities all clean :3");
+    
+    JoltConsole::logInfo("Init", "Cleanup complete.");
     
     SDL_Quit();
 }
