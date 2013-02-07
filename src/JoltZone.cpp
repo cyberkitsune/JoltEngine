b86181fdd2bde7e98073dad4be23a5307121dc47
Sean McClenaghan <cyberkitsune09@gmail.com>
2013-02-06 20:37:38 -0800

    JoltZone clusters added. See the comment on the source file for more info.

---
+++ src/JoltZone.cpp
@@ -0,0 +1,91 @@
+/* 
+ * File:   JoltZone.cpp
+ * Author: cyberkitsune
+ * 
+ * So this one needs some explaining...
+ * 
+ * A "JoltZone" as a cluster of maps. Think Megaman / Zelda Style.
+ * Our camera class utilizes this to pan and such. At the moment for debug,
+ * only 4 maps are currently rendered at a time.
+ * 
+ * More to come.
+ */
+
+#include <vector>
+
+#include "JoltZone.h"
+#include "DrawMacros.h"
+
+JoltZone JoltZone::zoneControl;
+
+JoltZone::JoltZone() {
+    areaSize = 0;
+}
+
+bool JoltZone::load(char* file) {
+    maps.clear();
+    
+    FILE* fileH = fopen(file, "r");
+    
+    if(fileH == NULL) {
+        //NOPE
+        return false;
+    }
+    
+    char tilesetFile[255];
+    
+    fscanf(fileH, "%s\n", tilesetFile);
+    
+    if((sTileset = DrawMacros::loadImage(tilesetFile)) == false) {
+        fclose(fileH);
+        
+        return false;
+    }
+    
+    fscanf(fileH, "%d\n", &areaSize);
+    
+    for (int X = 0; X < areaSize; X++) {
+        for (int Y = 0; Y < areaSize; Y++) {
+            char mapFile[255];
+            
+            fscanf(fileH, "%s ", mapFile);
+            
+            JoltMap tempMap;
+            if(tempMap.load(mapFile) == false) {
+                fclose(fileH);
+                
+                return false;
+            }
+            
+            tempMap.sTileset = sTileset;
+            maps.push_back(tempMap);
+        }
+        fscanf(fileH,"\n");
+
+
+    }
+    fclose(fileH);
+    
+    return true;
+    
+}
+
+void JoltZone::onRender(SDL_Surface* sDisplay, int camX, int camY) {
+    int mapWidth = MAP_WIDTH * TILE_SIZE;
+    int mapHeight = MAP_HEIGHT * TILE_SIZE;
+    
+    int FirstID = -camX / mapWidth;
+        FirstID = FirstID + ((-camY / mapHeight) * areaSize);
+        
+        for (int i = 0; i < 4; i++) {
+            int ID = FirstID + ((i / 2) * areaSize) + (i % 2);
+            
+            if(ID < 0 || ID >= maps.size()) continue;
+            
+            int x = ((ID % areaSize) * mapWidth) + camX;
+            int y = ((ID / areaSize) * mapHeight) + camY;
+            
+            maps[ID].onRender(sDisplay,x,y);
+    }
+
+}
---
+++ src/JoltZone.h
@@ -0,0 +1,35 @@
+/* 
+ * File:   JoltZone.h
+ * Author: cyberkitsune
+ *
+ * Created on February 6, 2013, 8:12 PM
+ */
+
+#ifndef JOLTZONE_H
+#define	JOLTZONE_H
+
+#include "JoltMap.h"
+
+
+class JoltZone {
+public:
+    static JoltZone             zoneControl;
+public:
+    std::vector<JoltMap>        maps;
+    
+private:
+    int                         areaSize;
+    
+    SDL_Surface*                sTileset;
+public:
+    JoltZone();
+    
+    bool load(char* file);
+    void onRender(SDL_Surface* sDisplay, int camX, int camY);
+    void onCleanup();
+
+
+};
+
+#endif	/* JOLTZONE_H */
+
