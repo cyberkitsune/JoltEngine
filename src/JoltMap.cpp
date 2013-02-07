ce2ade4bdcdf889141e52d090c012c7e9cb29df4
Sean McClenaghan <cyberkitsune09@gmail.com>
2013-02-06 20:07:12 -0800

    Implement JoltMap class :3

---
+++ src/JoltMap.cpp
@@ -0,0 +1,78 @@
+/* 
+ * File:   JoltMap.cpp
+ * Author: cyberkitsune
+ * 
+ * Created on February 6, 2013, 7:47 PM
+ */
+
+#include <vector>
+#include <stdio.h>
+
+#include "JoltMap.h"
+#include "DrawMacros.h"
+
+JoltMap::JoltMap() {
+    //Init the tileset
+    sTileset = NULL;
+}
+
+bool JoltMap::load(char* file) {
+    tileList.clear();
+
+    FILE* fileH = fopen(file, "r");
+
+    if (fileH == NULL) {
+        // Shit went wrong
+        return false;
+    }
+
+    // GOTTA MOVE THAT GEAR UP
+    for (int Y = 0; Y < MAP_HEIGHT; Y++) {
+        for (int X = 0; X < MAP_WIDTH; X++) {
+            JoltTile tempTile;
+
+            fscanf(fileH, "%d:%d ", &tempTile.tileID, &tempTile.typeID);
+            tileList.push_back(tempTile);
+
+        }
+        fscanf(fileH, "\n");
+
+    }
+    fclose(fileH);
+    
+    return true;
+}
+
+// Here goes nothing...
+// FASTEN YOUR SEATBELTS
+void JoltMap::onRender(SDL_Surface* sDisplay, int mapX, int mapY) {
+    if(sTileset == NULL) return; // We've gone too far, but we can't continue...
+    
+    int tilesetWidth = sTileset->w / TILE_SIZE;
+    //int tilesetHeight = sTileset->h / TILE_SIZE; // Don't need this.
+    
+    int ID = 0;
+    
+    for (int Y = 0; Y < MAP_HEIGHT; Y++) {
+        for (int X = 0; X < MAP_WIDTH; X++) {
+            if(tileList[ID].typeID == TILE_TYPE_NONE) {
+                ID++;
+                continue;
+            }
+
+            int tX = mapX + (X * TILE_SIZE);
+            int tY = mapY + (Y * TILE_SIZE);
+            
+            int tilesetX = (tileList[ID].tileID % tilesetWidth) * TILE_SIZE;
+            int tilesetY = (tileList[ID].tileID / tilesetWidth) * TILE_SIZE;
+
+            DrawMacros::doDraw(sDisplay, sTileset, tX, tY, tilesetX, tilesetY, TILE_SIZE, TILE_SIZE);
+        }
+
+
+
+    }
+
+    
+}
+
---
+++ src/JoltMap.h
@@ -0,0 +1,30 @@
+/* 
+ * File:   JoltMap.h
+ * Author: cyberkitsune
+ *
+ * Created on February 6, 2013, 7:47 PM
+ */
+
+#ifndef JOLTMAP_H
+#define	JOLTMAP_H
+
+#include "JoltTile.h"
+
+
+class JoltMap {
+public:
+    SDL_Surface* sTileset;
+    
+private:
+    std::vector<JoltTile> tileList;
+    
+public:
+    JoltMap();
+
+public:
+    bool load(char* file);
+    void onRender(SDL_Surface* sDisplay, int mapX, int mapY);
+};
+
+#endif	/* JOLTMAP_H */
+
